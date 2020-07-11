58d64f528045dadc1ce718aa7e5abc1549e60242
Joshua Tuscan <hello@joshuatuscan.com>
2020-07-11 18:06:26 -0400

    pushing the store id through a route

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -8,9 +8,10 @@ class StorePicker extends React.Component {
     //1. Stop the form from submitting
     event.preventDefault();
     //2. get the text from that input
-    console.log(this.myInput);
+    const storeName = this.myInput.current.value;
 
     //3. Change the page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
   };
 
   render() {
