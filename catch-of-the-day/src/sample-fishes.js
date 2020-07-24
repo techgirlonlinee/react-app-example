ff8e1f8c61b65449ac429a513db16579fb34a860
araya <hi@arayaaa.com>
2020-07-24 20:49:54 +0200

    swapped the pictures

--- catch-of-the-day/src/sample-fishes.js
+++ catch-of-the-day/src/sample-fishes.js
@@ -2,7 +2,8 @@
 const fishes = {
   fish1: {
     name: "GAY VILLAIN!",
-    image: "/images/hali.jpg",
+    // image: "/images/hali.jpg",
+    image: "/images/first.png",
     desc: "All Proceeds Go To the National Black trans Advocacy Coalition",
     price: 6000,
     creator: "JORDAN PARVEX",
@@ -11,7 +12,8 @@ const fishes = {
 
   fish2: {
     name: "DON'T SWEAT",
-    image: "/images/lobster.jpg",
+    // image: "/images/lobster.jpg",
+    image: "/images/second.png",
     desc: "Don't sweat it. Don't. Damn it!",
     price: 6000,
     creator: "MIRUNA A.",
@@ -20,7 +22,8 @@ const fishes = {
 
   fish3: {
     name: "PALAWAN HOT",
-    image: "/images/scallops.jpg",
+    // image: "/images/scallops.jpg",
+    image: "/images/third.png",
     desc: "Limited edition from illustrator and motion graphic artist",
     price: 4000,
     creator: "VALENTIN LAŽAR",
@@ -29,7 +32,8 @@ const fishes = {
 
   fish4: {
     name: "BEAUTIFUL LIFE",
-    image: "/images/mahi.jpg",
+    // image: "/images/mahi.jpg",
+    image: "/images/sixth.png",
     desc: "There is still so much in the world worth fighting for",
     price: 5500,
     creator: "JAMIE TALLERICO",
@@ -38,7 +42,8 @@ const fishes = {
 
   fish5: {
     name: "QUARANTINE 2020",
-    image: "/images/crab.jpg",
+    // image: "/images/crab.jpg",
+    image: "/images/fourth.png",
     desc: "Quarantine 2020 World Tour. Stay at home. Save lives.",
     price: 5000,
     creator: "KING SMITH",
@@ -47,7 +52,8 @@ const fishes = {
 
   fish6: {
     name: "ALLERGIC CLUB",
-    image: "/images/salmon.jpg",
+    // image: "/images/salmon.jpg",
+    image: "/images/fifth.png",
     desc: "What Doesn't Kill You Makes You Feel Worse",
     price: 4500,
     creator: "MATOUŠ MARTÁK",
@@ -56,7 +62,8 @@ const fishes = {
 
   fish7: {
     name: "DISPOSABLES",
-    image: "/images/oysters.jpg",
+    // image: "/images/oysters.jpg",
+    image: "/images/seven.png",
     desc: "For all my analog photographers",
     price: 4000,
     creator: "ALEXANDRA MOCANU",
@@ -65,7 +72,8 @@ const fishes = {
 
   fish8: {
     name: "A BETTER WORLD",
-    image: "/images/mussels.jpg",
+    // image: "/images/mussels.jpg",
+    image: "/images/eight.png",
     desc:
       "It is possible, and yes, there will be vultures in the speculative future",
     price: 4250,
@@ -75,7 +83,8 @@ const fishes = {
 
   fish9: {
     name: "SOLITUDE",
-    image: "/images/prawns.jpg",
+    // image: "/images/prawns.jpg",
+    image: "/images/nine.png",
     desc: "You're not your thoughts",
     price: 4250,
     creator: "JAWHEL",
